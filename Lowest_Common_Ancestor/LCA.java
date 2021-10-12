89abcd829cfae3de7b0c8b6ac52f91d27ddcf39a
Kjain2021 <khushboojain236@gmail.com>
2021-10-12 21:31:33 +0100

    Adding JUnit Test

--- Lowest_Common_Ancestor/LCA.java
+++ Lowest_Common_Ancestor/LCA.java
@@ -1,71 +1,96 @@
 import java.util.Scanner;
 
-class Node {
-    Node left;
-    Node right;
-    int data;
-
-    Node(int data) {
-        this.data = data;
-        left = null;
-        right = null;
-    }
-}
-
 class lca {
+    class Node {
+        Node left;
+        Node right;
+        int data;
 
-    public static Node insert(Node root, int data) {
+        Node(int data) {
+            this.data = data;
+            left = null;
+            right = null;
+        }
+    }
+
+    Node root;
+
+    // intialise root
+    lca() {
+        root = null;
+    }
+
+    void insert(int data) {
+        root = insert_RecursiveNode(root, data);
+    }
+
+    Node insert_RecursiveNode(Node root, int data) {
+        // tree empty
         if (root == null) {
-            return new Node(data);
+            root = new Node(data);
+            return root;
+            // traverse tree
         } else {
             Node temp;
             if (data <= root.data) {
-                temp = insert(root.left, data);
+                temp = insert_RecursiveNode(root.left, data);
                 root.left = temp;
             } else {
-                temp = insert(root.right, data);
+                temp = insert_RecursiveNode(root.right, data);
                 root.right = temp;
             }
             return root;
         }
     }
 
-    public static Node lca(Node root, int search1, int search2) {
+    public static Node lcaSol(Node root, int search1, int search2) {
         if (root == null) {
             return null;
-        } else if (root.data == search1 || root.data == search2)
-
-        {
+        } else if (root.data == search1 || root.data == search2) {
             return root;
         }
         // lca is on left subtree
         else if (root.data > search1 && root.data > search2) {
-            return lca(root.left, search1, search2);
+            return lcaSol(root.left, search1, search2);
         }
         // lca is on right
         else if (root.data < search1 && root.data < search2) {
-            return lca(root.right, search1, search2);
+            return lcaSol(root.right, search1, search2);
         }
         return root;
     }
 
     // as per the constrains no value shall be repeated in the tree and nodes need
     // to be unique.
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        System.out.print("Please enter number of nodes first, then followed by values.");
-        int numNodes = input.nextInt();
-        Node root = null;
-        while (numNodes-- > 0) {
-            int value = input.nextInt();
-            root = insert(root, value);
-            // System.out.print(root.data);
-        }
-        System.out.print("Enter the two value for which you want to find LCA:");
-        int search1 = input.nextInt();
-        int search2 = input.nextInt();
-        input.close();
-        Node lca = lca(root, search1, search2);
-        System.out.print(lca.data);
-    }
+    // public static void main(String[] args) {
+    // was done using user input where tree wasn't intialised.
+    // Scanner input = new Scanner(System.in);
+    // System.out.print("Please enter number of nodes first, then followed by
+    // values.");
+    // int numNodes = input.nextInt();
+    // Node root = null;
+    // while (numNodes-- > 0) {
+    // int value = input.nextInt();
+    // root = insert(root, value);
+    // // System.out.print(root.data);
+    // }
+    // System.out.print("Enter the two value for which you want to find LCA:");
+    // int search1 = input.nextInt();
+    // int search2 = input.nextInt();
+    // input.close();
+    // Node lca = lca(root, search1, search2);
+    // System.out.print(lca.data);
+
+    // intialise tree
+    // lca bst = new lca();
+    // System.out.print("Got intialised");
+    // bst.insert(45);
+    // bst.insert(10);
+    // bst.insert(7);
+    // bst.insert(12);
+    // bst.insert(90);
+    // bst.insert(50);
+    // Node resultNode = lcaSol(bst.root, 7, 90);
+    // System.out.print(resultNode.data);
+    // }
 }
---
+++ Lowest_Common_Ancestor/LCATest.java
@@ -0,0 +1,85 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class lcaTest {
+
+    @Test
+    public void testEmptyTree() {
+        lca bst1 = new lca();
+        // Empty tree, so the LCA doesn't exist and LCA doesn't exist as root is null
+        assertEquals("The LCA is null as root is null(empty tree)", null, lca.lcaSol(bst1.root, 4, 5));
+    }
+
+    @Test
+    public void testLCA() {
+
+        // 45
+        // / \
+        // 10 80
+        // /\ /
+        // 7 12 50
+        // Testing when the root is the LCA, incomplete tree, and
+        lca bst = new lca();
+        bst.insert(45);
+        bst.insert(10);
+        bst.insert(7);
+        bst.insert(12);
+        bst.insert(80);
+        bst.insert(50);
+        assertEquals("The lowest common ancestor of 7 and 80 is:", 45, (lca.lcaSol(bst.root, 7, 80).data));
+
+        // Testing when the tree is complete, LCA is root
+        // 45
+        // / \
+        // 10 80
+        // /\ / \
+        // 7 12 50 90
+        bst.insert(90);
+        assertEquals("The lowest common ancestor of 7 and 90 is:", 45, (lca.lcaSol(bst.root, 7, 90).data));
+
+        // Testing when the root is not the LCA, its on the right
+        assertEquals("The lowest common ancestor of 50 and 90 is:", 80, (lca.lcaSol(bst.root, 50, 90).data));
+
+        // Testing when the root is not the LCA, its on the left
+        assertEquals("The lowest common ancestor of 7 and 12 is:", 10, (lca.lcaSol(bst.root, 7, 12).data));
+
+        // Testing for the LCA, when one of the node is in left subtree and other on
+        // right subtree
+        assertEquals("The lowest common ancestor of 7 and 80 is:", 45, (lca.lcaSol(bst.root, 7, 80).data));
+
+    }
+
+    @Test
+    public void TestOneChild() {
+        // Testing when have one child
+        // 1
+        // \
+        // 2
+        // \
+        // 3
+        // \
+        // 4
+        lca bst2 = new lca();
+        bst2.insert(1);
+        bst2.insert(2);
+        bst2.insert(3);
+        bst2.insert(4);
+        assertEquals("The lowest common ancestor of 3 and 4 is:", 3, (lca.lcaSol(bst2.root, 3, 4).data));
+
+        // 2
+        // / \
+        // 1 3
+        // \
+        // 4
+
+        lca bst3 = new lca();
+        bst3.insert(2);
+        bst3.insert(1);
+        bst3.insert(3);
+        bst3.insert(4);
+        assertEquals("The lowest common ancestor of 1 and 3 is:", 2, (lca.lcaSol(bst3.root, 1, 3).data));
+
+    }
+
+}
